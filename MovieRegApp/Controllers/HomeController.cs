925861f3ef9bda130058c5acdbd115e50df77dc1
james <jamestheodorewatkins@gmail.com>
2020-05-22 01:10:24 -0400

    First commit for Movie Registration App

---
+++ MovieRegApp/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRegApp.Models;
+
+namespace MovieRegApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult MovieResult(Movie m)
+        {
+            return View(m);
+        }
+
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("MovieResult", m);
+            }
+            else
+            {
+                return View(m);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieRegApp/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRegApp.Models
+{
+    public class Movie
+    {
+        [Required (ErrorMessage = "Please enter an ID number")]
+        public int ID { get; set; }
+
+        [Required (ErrorMessage = "Please enter a Title")]
+        [MaxLength(50, ErrorMessage = "Title must be 50 characters or less") ]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        //[Range(typeof(DateTime), "1880-01-01", DateTime.Now.ToString(), ErrorMessage = "Date must be between 1880 and now")]
+        public DateTime ReleaseDate { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+    }
+}
